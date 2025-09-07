cbb547d77bc12332c97e9ea01d020f930d085bcf
Pablo Ferreiro <me@pabloferreiro.es>
2025-09-07 17:01:14 +0200

    added logo

--- templates/layouts/default.php
+++ templates/layouts/default.php
@@ -5,11 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <link rel="stylesheet" href="<?=$this->url('/css/main.css')?>">
     <title><?= $this->e($title) ?> - Onboarding CEETSII</title>
 </head>
 
 <body>
     <header>
+        <picture>
+            <source class="icon" srcset="<?=$this->url('/assets/logo-white.png')?>" media="(prefers-color-scheme: dark)" />
+            <source class="icon" srcset="<?=$this->url('/assets/logo-multi.png')?>" media="(prefers-color-scheme: light)" />
+            <img class="icon" src="<?=$this->url('/assets/logo-multi.png')?>" />
+        </picture>
         <h1>Onboarding CEETSII</h1>
         <nav>
             <ul>
